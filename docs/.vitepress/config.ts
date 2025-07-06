697b5a6295b2499b4dd2e40ebdcf9e09746a5aa1
rymiyamoto <ry.miyamoto129@gmail.com>
2025-07-06 12:27:34 +0000

    feat: add suffix

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   lang: 'ja',
   title: 'パンダムの徒然なる日記',
+  titleTemplate: ':title - パンダムの徒然なる日記 Suffix',
   description: 'ワイことパンダムがエンジニアチックなこと書くし、ただ趣味やオタクなことを何でも雑に書くブログです。',
 })

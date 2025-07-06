306b6016a06fd1f2696737b11d5c4ea956b66dff
rymiyamoto <ry.miyamoto129@gmail.com>
2025-07-06 12:37:29 +0000

    feat: update confg

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,4 +5,28 @@ export default defineConfig({
   title: 'パンダムの徒然なる日記',
   titleTemplate: ':title - パンダムの徒然なる日記 Suffix',
   description: 'ワイことパンダムがエンジニアチックなこと書くし、ただ趣味やオタクなことを何でも雑に書くブログです。',
+  head: [
+    [
+      'link',
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' }
+    ],
+    [
+      'link',
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }
+    ],
+    [
+      'link',
+      { href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap', rel: 'stylesheet' }
+    ],
+    [
+      'link',
+      { rel: 'icon', href: '/images/common/favicon.ico' }
+    ],
+  ],
+  ignoreDeadLinks: true,
+  themeConfig: {
+    nav: [
+      { text: 'About', link: '/about' },
+    ]
+  },
 })

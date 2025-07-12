de14825764c2c20795a9dfb713804f434288873b
rymiyamoto <ry.miyamoto129@gmail.com>
2025-07-12 19:01:33 +0000

    feat: move file

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,8 @@
 import DefaultTheme from 'vitepress/theme'
-import PostCardList from '../../../src/components/Post/PostCardList.vue'
-import PostCard from '../../../src/components/Post/PostCard.vue'
-import SNSLinks from '../../../src/components/About/SNSLinks.vue'
-import Profile from '../../../src/components/About/Profile.vue'
+import PostCardList from '../../src/components/Post/PostCardList.vue'
+import PostCard from '../../src/components/Post/PostCard.vue'
+import SNSLinks from '../../src/components/About/SNSLinks.vue'
+import Profile from '../../src/components/About/Profile.vue'
 
 export default {
   ...DefaultTheme,

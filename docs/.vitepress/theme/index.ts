c25dbd36d034d3a12cbbecb1e900a8a4cd1dee63
rymiyamoto <ry.miyamoto129@gmail.com>
2025-07-12 18:48:21 +0000

    feat: bundle about

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,15 @@
 import DefaultTheme from 'vitepress/theme'
-import './about.css'
-import PostCardList from '../../../src/components/PostCardList.vue'
-import PostCard from '../../../src/components/PostCard.vue'
+import PostCardList from '../../../src/components/Post/PostCardList.vue'
+import PostCard from '../../../src/components/Post/PostCard.vue'
+import SNSLinks from '../../../src/components/About/SNSLinks.vue'
+import Profile from '../../../src/components/About/Profile.vue'
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
     app.component('PostCardList', PostCardList)
     app.component('PostCard', PostCard)
+    app.component('SNSLinks', SNSLinks)
+    app.component('Profile', Profile)
   }
 }
---
+++ src/components/About/Profile.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="profile">
+    <img src="/images/common/icon.jpeg" alt="パンダム/rymiyamoto" class="profile-icon"/>
+    <p class="profile-text">
+      <a href="https://tabelog.com/tokyo/A1314/A131402/13123414/">鉄火</a>を実家と主張する人🍶<br>
+      Twitter(強い意志)が主戦場
+    </p>
+  </div>  
+</template>
+
+<style scoped>
+.profile {
+  display: flex;
+  align-items: center;
+  text-align: center;
+  margin-top: 16px;
+  gap: 16px;
+}
+
+.profile-icon {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+.profile-text {
+  text-align: left;
+  font-size: 16px;
+  line-height: 1.5;
+  color: var(--vp-c-text);
+}
+</style>
